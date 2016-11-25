36a6a18f82a730f0d0e8766fe5e7254d804e327a
Andriy Volkov <dev@andriy.dk>
2016-11-25 13:46:32 +0100

    Add files via upload

---
+++ JValue.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace JSReader
+{
+    public class JValue : JArrayItem
+    {
+        public JValue(string _value) { Value = _value; }
+
+        public override string JText { get { return $"\"{Value}\""; } }
+
+        public override string JTextLight { get { return $"{Value}"; } }
+
+        public override bool IsValid { get { return true; } }
+
+        public string Value { get; internal set; }
+    }
+}
